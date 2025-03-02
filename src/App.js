0e3ff12bfbddcc61fd20a6968911724883e72490
Rei <aloha.reirei1@gmail.com>
2025-03-02 13:36:40 -1000

    Task 3

--- src/App.js
+++ src/App.js
@@ -1,3 +1,34 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import { useState } from 'react';
+
+function Square() {
+  const [value, setValue] = useState(null);
+  return <button className="square" onClick={handleClick}>{value}</button>;
+
+    function handleClick() {
+      setValue('X');
+    }
+}
+
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
 }
